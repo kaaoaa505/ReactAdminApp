cf7494cf9e5fb5915ce15e521e9285cc267d37fe
Khaled Allam <kaaoaa505@gmail.com>
2023-03-15 08:31:37 +0300

    Divide App Component into Nav, Sidebar, and Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/style-prop-object */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import './App.css';
-import Dashboard from './components/Dashboard';
-import Nav from './components/Nav';
-import SidebarMenu from './components/SidebarMenu';
+import Dashboard from './secure/Dashboard';
+import Nav from './secure/components/Nav';
+import SidebarMenu from './secure/components/SidebarMenu';
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
 
           <SidebarMenu />
 
-          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4"><div className="chartjs-size-monitor"><div className="chartjs-size-monitor-expand"><div className=""></div></div><div className="chartjs-size-monitor-shrink"><div className=""></div></div></div>
+          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
             
             <Dashboard />
 
---
+++ src/secure/Users.tsx
@@ -0,0 +1,9 @@
+const Users = () => (
+    <div className="UsersComponent">
+
+        To do users component content
+
+    </div>
+);
+
+export default Users;
