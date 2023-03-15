7b7da789fb76682135e8c712c0b1ac40f83fea31
Khaled Allam <kaaoaa505@gmail.com>
2023-03-15 09:08:47 +0300

    put BrowserRouter directly under AppComponent div to enable Link from anywhere

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,48 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import './App.css';
 import Dashboard from './secure/Dashboard';
+import Users from './secure/Users';
 import Nav from './secure/components/Nav';
 import SidebarMenu from './secure/components/SidebarMenu';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Login from './public/Login';
 
 function App() {
   return (
     <div className="AppComponent">
-      
-      <Nav />
+      <BrowserRouter>
 
-      <div className="container-fluid">
-        <div className="row">
+        <Nav />
 
-          <SidebarMenu />
+        <div className="container-fluid">
+          <div className="row">
 
-          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            
-            <Dashboard />
+            <SidebarMenu />
 
-          </main>
+            <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+
+              <Link to="/">Dashboard</Link>
+              &nbsp; | &nbsp;
+              <Link to="/users">Users</Link>
+              &nbsp; | &nbsp;
+              <Link to="/login">Login</Link>
+
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/users" element={<Users />} />
+                <Route path="/login" element={<Login />} />
+              </Routes>
+
+            </main>
+          </div>
         </div>
-      </div>
+
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/public/Login.tsx
@@ -0,0 +1,35 @@
+import { Component } from "react";
+
+class Login extends Component {
+    render() {
+        return (
+            <div className="LoginComponent">
+
+                <form>
+                    <img className="mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57" />
+                    <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+                    <div className="form-floating">
+                        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                        <label htmlFor="floatingInput">Email address</label>
+                    </div>
+                    <div className="form-floating">
+                        <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                        <label htmlFor="floatingPassword">Password</label>
+                    </div>
+
+                    <div className="checkbox mb-3">
+                        <label>
+                            <input type="checkbox" value="remember-me" /> Remember me
+                        </label>
+                    </div>
+                    <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+                    <p className="mt-5 mb-3 text-muted">&copy; 2010-2023</p>
+                </form>
+
+            </div>
+        );
+    }
+}
+
+export default Login;
